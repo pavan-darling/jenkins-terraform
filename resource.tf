d9cb7f8017215457bf8da6192b394f9e29e554de
pavan-darling <pavangurrapusala208@gmail.com>
2020-12-12 08:12:11 +0530

    ec2 file

--- resource.tf
+++ resource.tf
@@ -1,6 +1,8 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  instance_tenancy = "default"
+resource "aws_instance" "terraform" {
+  ami  = "ami-05394aef61908afaa"
+  instance_type = "t2.micro" 
+  security_group_ID = "sg-0878e918be7ad41e1"
+  key_name   = "pavan208"
   tags= {
     Name = "demo-vpc"
   }
