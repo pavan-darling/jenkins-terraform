5d0d612d9960b15a115714e54f3a1ff1f75e4d69
pavan-darling <pavangurrapusala208@gmail.com>
2020-12-12 08:35:35 +0530

    fix

--- resource.tf
+++ resource.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "terraform" {
   ami  = "ami-05394aef61908afaa"
   instance_type = "t2.micro" 
-  security_group_ID = "sg-0878e918be7ad41e1"
   key_name   = "pavan208"
   tags= {
     Name = "demo-vpc"
